2c90553c7130f84bde122ff9136caa804110008d
rory.amarasekara@gmail.com <150187Hajime!>
2020-12-11 13:30:53 +0100

    add ReserveCalculationService

---
+++ src/main/java/service/ReserveCalculationService.java
@@ -0,0 +1,15 @@
+package service;
+
+public class ReserveCalculationService {
+
+     /* Context:
+        When a bank lends money through a contract, it takes a risk.
+        In order to reduce this risk, it must put aside a percentage of the total amount loaned.
+
+        Rule:
+        If (16 Million <total_amount< 120 Million) Then reserve_percentage = 3%
+        If (total_amount > 120 Million) Then reserve_percentage = 10%
+     */
+
+     //TODO develop the functionality required
+}
